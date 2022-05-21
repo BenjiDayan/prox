d29675946133ed1c8a87eeebefc25c442d03ceef
Mere19 <mere1998@outlook.com>
2022-05-21 15:40:45 +0200

    merge

--- virtual_huams_resource/utils.py
+++ virtual_huams_resource/utils.py
@@ -1,10 +1,8 @@
 from glob import glob
 import torch
 from pytorch3d import transforms
-import pickle
-from pathlib import Path
-import smplx
-import tqdm
+import numpy as np
+import scipy.ndimage as img_fn
 
 
 def normalize_euler_angles(base_rot_matrix, euler_angles):
@@ -43,3 +41,31 @@ def extract_data(data_dict):
     global_orient = torch.Tensor(data_dict['global_orient'])
     transl = torch.Tensor(data_dict['transl'])
     return betas, body_pose, global_orient, transl
+
+def l1_inpainting(f, mask, theta=0.001, maxIter=5000):
+    u = np.array((1 - mask) * f)
+    mask = np.pad(mask, pad_width=1, mode='edge')
+    f = np.pad(f, pad_width=1, mode='edge')
+
+    for i in range(maxIter):
+        u_b = np.pad(u, pad_width=1, mode='edge')
+        ux = img_fn.sobel(u, axis=1)
+        uy = img_fn.sobel(u, axis=0)
+        phi_prime = np.pad( 1 /np.sqrt(1 + (ux**2 +uy**2 ) /theta**2), pad_width=1, mode='edge')
+
+        u_bp = np.roll(u_b, 1, axis=1)
+        u_bn = np.roll(u_b, -1, axis=1)
+        u_bu = np.roll(u_b, 1, axis=0)
+        u_bd = np.roll(u_b, -1, axis=0)
+
+        a_p = 0.5 *(phi_prime + np.roll(phi_prime, 1, axis=1))
+        a_n = 0.5 *(phi_prime + np.roll(phi_prime, -1, axis=1))
+        a_u = 0.5 *(phi_prime + np.roll(phi_prime, 1, axis=0))
+        a_d = 0.5 *(phi_prime + np.roll(phi_prime, -1, axis=0))
+        a_c = (a_p + a_n + a_u + a_d)
+
+        u_c = ((1 - mask) * f + 1.0 * mask * (a_p * u_bp + a_n * u_bn + a_u * u_bu + a_d * u_bd)
+               ) / (1 - mask + 1.0 * mask * a_c)
+        u = np.copy(u_c[1:-1, 1:-1])
+
+    return u
